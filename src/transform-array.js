45dee5a85b52a5a0a58b64c2de1103bf64b22f0a
Falxns <maks.falonse@gmail.com>
2021-03-20 11:47:00 +0300

    feat: add solution to transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!arr instanceof Array) {
+    throw Error;
+  }
+  let res = [];
+  let isDiscarded = false;
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case "--discard-next": {
+        if (arr[i + 1] !== undefined) {
+          i++;
+          isDiscarded = true;
+        }
+        break;
+      }
+      case "--discard-prev": {
+        if (arr[i - 1] !== undefined && !isDiscarded) {
+          res.pop();
+        }
+        break;
+      }
+      case "--double-next": {
+        if (arr[i + 1] !== undefined) {
+          res.push(arr[i + 1]);
+        }
+        break;
+      }
+      case "--double-prev": {
+        if (arr[i - 1] !== undefined && !isDiscarded) {
+          res.push(arr[i - 1]);
+        }
+        break;
+      }
+      default: {
+        isDiscarded = false;
+        res.push(arr[i]);
+        break;
+      }
+    }
+  }
+  return res;
 };

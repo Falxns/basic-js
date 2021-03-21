376474664faa424d0615dcb8922eecbed97d9d2d
Falxns <maks.falonse@gmail.com>
2021-03-21 18:51:21 +0300

    feat: add solution to recursive-depth task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr, depth) {
+    if (!depth) {
+      depth = 0;
+    }
+    if (arr.length === 0) {
+      depth++;
+      return depth;
+    } else {
+      depth++;
+      let array = [];
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i] instanceof Array) {
+          array.push(this.calculateDepth(arr[i], depth));
+        }
+      }
+      if (depth < Math.max(...array)) {
+        return Math.max(...array);
+      } else {
+        return depth;
+      }
+    }
   }
 };

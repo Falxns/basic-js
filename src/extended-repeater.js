7ee4226e8d9789747d621a492ea71df360bb8693
Falxns <maks.falonse@gmail.com>
2021-03-21 19:56:49 +0300

    feat: add solution to extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,69 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (!str instanceof String) {
+    str = str.toString();
+  }
+  let res = "";
+  if (options.repeatTimes !== undefined) {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      res += str;
+      if (options.additionRepeatTimes !== undefined) {
+        for (let j = 0; j < options.additionRepeatTimes; j++) {
+          if (options.addition !== undefined) {
+            if (!options.addition instanceof String) {
+              options.addition = options.addition.toString();
+            }
+            res += options.addition;
+            if (options.additionSeparator === undefined) {
+              options.additionSeparator = "|";
+            }
+            if (j !== options.additionRepeatTimes - 1) {
+              res += options.additionSeparator;
+            }
+          }
+        }
+      } else {
+        if (options.addition !== undefined) {
+          if (!options.addition instanceof String) {
+            options.addition = options.addition.toString();
+          }
+          res += options.addition;
+        }
+      }
+      if (options.separator === undefined) {
+        options.separator = "+";
+      }
+      if (i !== options.repeatTimes - 1) {
+        res += options.separator;
+      }
+    }
+  } else {
+    res += str;
+    if (options.additionRepeatTimes !== undefined) {
+      for (let j = 0; j < options.additionRepeatTimes; j++) {
+        if (options.addition !== undefined) {
+          if (!options.addition instanceof String) {
+            options.addition = options.addition.toString();
+          }
+          res += options.addition;
+          if (options.additionSeparator === undefined) {
+            options.additionSeparator = "|";
+          }
+          if (j !== options.additionRepeatTimes - 1) {
+            res += options.additionSeparator;
+          }
+        }
+      }
+    } else {
+      if (options.addition !== undefined) {
+        if (!options.addition instanceof String) {
+          options.addition = options.addition.toString();
+        }
+        res += options.addition;
+      }
+    }
+  }
+
+  return res;
 };
-  
